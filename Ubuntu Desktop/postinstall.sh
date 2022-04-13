53e0108f0b4defc88071b8ceaf8b4cf78e660698
Egor Repiev <33565251+ega22a@users.noreply.github.com>
2022-04-13 15:19:35 +0500

    ФИКС

--- Ubuntu Desktop/postinstall.sh
+++ Ubuntu Desktop/postinstall.sh
@@ -77,7 +77,7 @@ else
 	ethernet_iface="$(ls /sys/class/net | grep en)"
 	# ethernet_mac="$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address)"
 	# \n      match:\n        macaddress: %s\n
-	printf "network:\n  version: 2\n  renderer: NetworkManager\n  ethernets:\n    %s:      wakeonlan: true\n      dhcp4: no\n      addresses: [%s/%s]\n      gateway4: %s\n      nameservers:\n        addresses: [%s]\n" $ethernet_iface $ipAddress $ipMask $ipGateway $ipDNS > /etc/netplan/nttek-netplan.yaml
+	printf "network:\n  version: 2\n  renderer: NetworkManager\n  ethernets:\n    %s:\n      wakeonlan: true\n      dhcp4: no\n      addresses: [%s/%s]\n      gateway4: %s\n      nameservers:\n        addresses: [%s]\n" $ethernet_iface $ipAddress $ipMask $ipGateway $ipDNS > /etc/netplan/nttek-netplan.yaml
 	netplan apply
 
 	apt -y --force-yes install sssd sssd-ldap ldap-utils openssh-server
